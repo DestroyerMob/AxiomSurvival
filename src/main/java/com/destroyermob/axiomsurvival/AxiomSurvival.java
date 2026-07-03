8a6d01ec4e6eb2dbc39d2c6ff6e3fdd44a826c83
DestroyerMob <ethanhellyer7@gmail.com>
2026-07-03 17:02:45 +1000

    Avoid Fabric API dependency for command registration

--- src/main/java/com/destroyermob/axiomsurvival/AxiomSurvival.java
+++ src/main/java/com/destroyermob/axiomsurvival/AxiomSurvival.java
@@ -1,7 +1,6 @@
 package com.destroyermob.axiomsurvival;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -12,7 +11,5 @@ public final class AxiomSurvival implements ModInitializer {
     @Override
     public void onInitialize() {
         AxiomSurvivalConfig.load();
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) ->
-                AxiomSurvivalCommands.register(dispatcher));
     }
 }
